e4516ebd330289fb6ef39bebc616d289d10988e9
Victor M. Suarez <svmnotn@gmail.com>
2015-12-29 15:21:40 -0500

    add clap and start figuring how the ledger program will work

--- src/bin/ledger.rs
+++ src/bin/ledger.rs
@@ -1,6 +1,18 @@
 extern crate ardite;
+#[macro_use]
+extern crate clap;
 
-fn main(){
-  println!("Hello World! I am ardite-ledger!");
-  ardite::libs();
+use clap::App;
+
+fn main() {
+  let matches = App::new("ledger")
+                  .version(&crate_version!())
+                  .global_version(true)
+                  .unified_help_message(true)
+                  .author("Victor M. Suarez <svmnotn@gmail.com>\nCaleb Meredith <calebmeredith8@gmail.com>")
+                  .about("TODO")
+                  .args_from_usage("--directory=[path] -d 'where the data is on disk (defaults to the current dir)'")
+                  .get_matches();
+  let dir = matches.value_of("path").unwrap_or(".");
+  println!("{}", dir);
 }
